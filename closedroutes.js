c10f27e48a02be44616013c454315c7a8b0adfa1
Jacob Bryan <jtb36688@gmail.com>
2019-02-26 17:13:49 -0500

    Added logout feature, session configuration

---
+++ closedroutes.js
@@ -0,0 +1,10 @@
+// const express = require("express");
+// const db = require("./data/user-model.js");
+// const router = express.Router();
+
+// router.get("/api/restricted/check", (req, res) => {
+//     res.send(`You're Logged in! You sent ${JSON.stringify(req.body)}`)
+//   });
+
+
+// module.exports = router;
--- index.js
+++ index.js
@@ -1,28 +1,78 @@
 const express = require("express");
-const db = require("./data/user-model.js");
-const bcrypt = require('bcryptjs')
-const cors= require('cors')
+const bcrypt = require("bcryptjs");
+const cors = require("cors");
+const session = require("express-session");
+// const closedroutes = require('./closedroutes.js')
+const KnexSessionStore = require('connect-session-knex')(session)
 
+const db = require("./data/user-model.js");
+dbConfig = require("./data/dbConfig.js")
 
 const server = express();
+const sessionConfig = {
+  name: "rockruff",
+  secret: "woofbarkwoof",
+  cookie: {
+    maxAge: 1000 * 60 * 15, //value in milliseconds, 15 minutes
+    secure: false //use over https only?
+  },
+  httpOnly: true, // cannot acces the cookie from js using document.cookie
+  resave: false,
+  saveUninitialized: false, // GDPR laws against setting cookies automatically,
+  store: new KnexSessionStore({
+    knex: dbConfig,
+    tablename: 'sessions',
+    sidfieldname: 'sid',
+    createtable: true,
+    clearInterval: 1000 * 60 * 60, // value in milliseconds, 60 minutes
+
+  })
+};
 
 server.use(express.json());
-server.use(cors())
+server.use(cors());
+// server.use("/api/restricted", restricted, closedroutes)
+server.use(session(sessionConfig));
+server.use("/api/restricted", restricted);
 
 server.post("/api/register", (req, res) => {
   let user = req.body;
   const hash = bcrypt.hashSync(user.password, 12);
   user.password = hash;
   db.add(user)
-    .then(add => {
-      res.status(201).json(add);
+    .then(saved => {
+      req.session.user = saved;
+      res.status(201).json(saved);
     })
     .catch(({ code, message }) => {
       res.status(code).json({ message });
     });
 });
 
-server.post("/api/login", (req, res) => {});
+server.post("/api/login", (req, res) => {
+  const { username, password } = req.body;
+
+  if (username && password) {
+    db.findBy({ username })
+      .first()
+      .then(user => {
+        // check that passwords match
+        if (user && bcrypt.compareSync(password, user.password)) {
+          req.session.user = user;
+          res
+            .status(200)
+            .json({
+              message: `Welcome ${user.username}, providing cookie now.`
+            });
+        } else {
+          res.status(401).json({ message: "Login Error" });
+        }
+      })
+      .catch(error => {
+        res.status(500).json(error);
+      });
+  }
+});
 
 server.get("/api/users", restricted, (req, res) => {
   db.get()
@@ -34,27 +84,54 @@ server.get("/api/users", restricted, (req, res) => {
     });
 });
 
-function restricted(req, res, next) {
-  const { username, password } = req.headers;
+server.get("/api/logout", (req, res) => {
+  if (req.session) {
+    req.session.destroy(err => {
+      if (err) {
+      } else {
+      }
+    });
+  } else {
+    res.end();
+  }
+});
 
-  if (username && password) {
-    db.findBy({ username })
-      .first()
-      .then(user => {
-        // check that passwords match
-        if (user && bcrypt.compareSync(password, user.password)) {
-          next();
-        } else {
-          res.status(401).json({ message: "You shall not pass!" });
-        }
-      })
-      .catch(error => {
-        res.status(500).json(error);
-      });
+// function restricted(req, res, next) {
+//   const { username, password } = req.headers;
+
+//   if (username && password) {
+//     db.findBy({ username })
+//       .first()
+//       .then(user => {
+//         // check that passwords match
+//         if (user && bcrypt.compareSync(password, user.password)) {
+//           next();
+//         } else {
+//           res.status(401).json({ message: "You shall not pass!" });
+//         }
+//       })
+//       .catch(error => {
+//         res.status(500).json(error);
+//       });
+//   }
+// }
+
+function restricted(req, res, next) {
+  if (req.session && req.session.user) {
+    next();
+  } else {
+    res.status(401).json({ message: "No session Found" });
   }
 }
-// server.use('/api/restricted', restricted, restrictedrouters)
 
 
-  const port = process.env.PORT || 5000;
-  server.listen(port, () => console.log(`\n** Running on port ${port} **\n`));
+
+server.get("/api/restricted/check", (req, res) => {
+  res.send(`You're Logged in! You sent ${JSON.stringify(req.body)}`);
+});
+
+// This works as long as the server.use comes before the route handler to the
+// related route.
+
+const port = process.env.PORT || 5000;
+server.listen(port, () => console.log(`\n** Running on port ${port} **\n`));
